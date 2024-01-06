a8e903ef881f1d66c85005822e46e11c237a4f51
Hana Bouikni <hanabouikni42@gmail.com>
2024-01-07 00:41:39 +0100

    j'ai moddifier la declaration de la structures de lenregistrement et j'ai ajoutée la fonction d'ouverture du fichier

--- Naila.c
+++ Naila.c
@@ -6,30 +6,52 @@
  }buffer,bloc;
 //ENTETE 
 typedef struct Entete{
+    int nb_btete;//le numero de premier bloc
     int nbbloc;
     int nbenreg;
 }Entete;
-//declaration de la strecture enregistrement 
+//declaration de la structure de l'enregistrement 
 typedef struct enreg {
- typedef struct lenreg *enreg{
  lenreg  *svt ;
-    char nom[10];
-    char prenom[10];     }lenreg;
-}enreg;
+    char marque[10];
+    char modele[100];
+    char moteur[100]; 
+    char reference[100];    }lenreg;
  
 typedef struct  LOF{
 Entete entete;
-
-FILE *NIH=NULL;
+FILE *NIH ;
  }LOF;
-   
-    
+
+//FONCTION QUI OUVRE LE FICHIER
+LOF* OPEN (char filename[100],const char mode)
+{
+LOF (*FILE )=malloc(sizeof(LOF));
+if ((mode=='n')||(mode=='N'))//si le mode est nouveau (n,N)
+    {   FILE->NIH=fopen(filename,"wb+");
+        FILE->entete.nb_btete=0;
+        FILE->entete.nbenreg=0; 
+        FILE->entete.nbbloc=0;
+        fwrite(&(FILE->entete),sizeof(Entete),1,FILE->NIH);
+    }
+    else
+    {
+        if ((mode=='a')||(mode=='A'))//si le mode est ancien (a,A)
+        {
+          FILE->NIH=fopen(filename,"rb+");
+          fread(&(FILE->entete),sizeof(Entete),1,FILE->NIH);
+        }
+        else printf("incorrect mode");
+    }
+    return FILE;
+}
+
+  
 int main()
 {
-    
-
-   NIH = fopen("employer.txt","r+");
-   if (NIH== NULL)
+    LOF *f ;
+     f->NIH = fopen("AUDI.txt","r+");
+   if (f->NIH== NULL)
    {
        printf("ERREUR !!!!\n");
        return -1;
